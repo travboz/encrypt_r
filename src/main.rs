ac1740b755c1b34d00fbb7a7b14949ceca651b64
Travis Bozic <z5368318@ad.unsw.edu.au>
2024-03-27 14:41:05 +1100

    Refactor encryption step out into methods and restructure directories

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,104 @@
+// default alphabet
 const PUNCTUATION: &str = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 const DIGITS: &str = "0123456789";
 const ASCII_LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const SPACE: &str = " ";
 
-// use rand::Rng;
 use rand::rngs::mock::StepRng;
+use rand::Rng;
+
 use shuffle::irs::Irs;
 use shuffle::shuffler::Shuffler;
+use std::error::Error;
+use std::io;
 
-fn main() {
-    let mut rng = StepRng::new(2, 11);
-    let mut irs = Irs::default();
+struct Encrypter {
+    alphabet: Vec<String>,
+    cipher: Vec<String>,
+}
 
-    let options = format!("{}{}{} ", PUNCTUATION, DIGITS, ASCII_LETTERS);
+impl Encrypter {
+    fn new() -> io::Result<Self> {
+        // added extra random factor -> because each cipher was identifcal
+        let mut rng = rand::thread_rng();
+        let initial: u64 = rng.gen();
 
-    // let mut input = vec![1, 2, 3, 4, 5];
+        // creating normal alphabet
+        let default_alphabet = [ASCII_LETTERS, PUNCTUATION, DIGITS, SPACE].concat();
+        let alphabet = default_alphabet
+            .chars()
+            .map(|c| c.to_string())
+            .collect::<Vec<String>>();
 
-    let chars = options
-        .chars()
-        .map(|c| c.to_string())
-        .collect::<Vec<String>>();
+        // creating cipher alphabet
+        let mut rng = StepRng::new(initial, 13); // picking range
+        let mut irs = Irs::default(); // using Inverse Riffle Shuffle to shuffle cipher alphabet
 
-    let mut key = options
-        .chars()
-        .map(|c| c.to_string())
-        .collect::<Vec<String>>();
+        // copying default for shuffling
+        let mut key = default_alphabet
+            .chars()
+            .map(|c| c.to_string())
+            .collect::<Vec<String>>();
 
-    let input = "The quick brown fox jumps over the lazy dog"
-        .chars()
-        .map(|c| c.to_string())
-        .collect::<Vec<String>>();
+        let _shuffled = irs.shuffle(&mut key, &mut rng);
 
+        Ok(Self {
+            alphabet,
+            cipher: key,
+        })
+    }
+
+    // fn encrypt(s: String) -> {
+
+    // }
+
+    // for future implementation: choosing your own alphabet
+    // fn change_alphabet(&self, words: &[&str]) -> Vec<String> {
+    //     let alphabet = words.concat();
+
+    //     alphabet
+    //         .chars()
+    //         .map(|c| c.to_string())
+    //         .collect::<Vec<String>>()
+
+    // will then need to update cipher alphabet
+    // }
+
+    fn get_user_input(prompt: &str) -> io::Result<Vec<String>> {
+        println!("{}", prompt);
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        let input = input.trim().to_string();
+        Ok(input
+            .chars()
+            .map(|c| c.to_string())
+            .collect::<Vec<String>>())
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let enc = Encrypter::new()?;
+
+    let input = Encrypter::get_user_input("Enter a string you wish to encrypt: ")?;
+
+    // encrypt input
     let mut cipher_text = String::new();
 
-    let shuffled = irs.shuffle(&mut key, &mut rng);
-
     for letter in &input {
-        if let Some(index) = chars.iter().position(|x| x == letter) {
+        if let Some(index) = enc.alphabet.iter().position(|x| x == letter) {
             // println!("letter {} is at index {} in original string", letter, index);
-            cipher_text.push_str(&key[index]);
+            cipher_text.push_str(&enc.cipher[index]);
         } else {
             println!("letter '{}' not found", letter);
         }
     }
 
+    println!("Input text is now: {:?}", input.join(""));
     println!("Cipher text is now: {cipher_text}");
 
-    // Decryption
+    // Decrypt input
     let mut plain_text = String::new();
 
     let cipher_iter = cipher_text
@@ -54,8 +107,8 @@ fn main() {
         .collect::<Vec<String>>();
 
     for letter in &cipher_iter {
-        if let Some(index) = key.iter().position(|x| x == letter) {
-            plain_text.push_str(&chars[index]);
+        if let Some(index) = enc.cipher.iter().position(|x| x == letter) {
+            plain_text.push_str(&enc.alphabet[index]);
         } else {
             println!("letter '{}' not found", letter);
         }
@@ -63,4 +116,6 @@ fn main() {
 
     println!("Cipher text is now: {cipher_text}");
     println!("Original text is now: {plain_text}");
+
+    Ok(())
 }
